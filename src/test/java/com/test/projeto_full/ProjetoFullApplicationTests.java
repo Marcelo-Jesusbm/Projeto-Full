186eca6ff59cfa751e494b46d34a80c80eda4082
Marcelo Jesus <mhsj.4001@gmail.com>
2026-02-12 18:15:41 -0300

    projeto iniciado

---
+++ src/test/java/com/test/projeto_full/ProjetoFullApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.projeto_full;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoFullApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
